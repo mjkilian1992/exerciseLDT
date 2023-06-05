42707ca00a4b382acc8bb678dd24a777e66b975c
Michael Kilian <michael@allplants.com>
2023-06-05 16:47:25 +0100

    Initial

---
+++ main.py
@@ -0,0 +1,9 @@
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    import ipdb
+    ipdb.set_trace()
+    print('running')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
